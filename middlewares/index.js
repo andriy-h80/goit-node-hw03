fb12c0dd5fe251bc12ee041f1d6d15faa833e7f2
andriy-h80 <andrey.hanzel@gmail.com>
2023-07-11 18:02:21 +0300

    some changes

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,7 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
-const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
     validateBody,
     isValidId,
-    handleMongooseError,
 };
